5750d993e77aec05e802cd6bbed4f543d1c03632
aobiegb <aobiegb@us.ibm.com>
2020-11-06 21:49:47 -0500

    added insert index to array

--- arrays/DynamicArray.py
+++ arrays/DynamicArray.py
@@ -45,14 +45,23 @@ class DynamicArray:
         return self.data[index] # return item at index location
 
     
-    def insert(self, value):
+    def insert(self, value, index):
         """
         Inserts value to the end of the array
         """
+        if index < 0 or index > self.get_length():
+            raise OutOfBoundException("Index valud is out of bound")
+
         if self.get_length() == len(self.data):
             self._resize()
 
-        self.data[self.get_length()] = value
+
+        new_data = self.data[index:self.get_length()]
+        self.data[index] = value
+
+        for i in range(index+1, self.get_length()+1):
+            self.data[i] = new_data[i-(index+1)]
+
         self.size += 1
 
     
@@ -105,17 +114,70 @@ class DynamicArray:
             new_data[i] = self.data[i]
         
         self.data = new_data
+
+
+def rearrange(arr):
+    """
+    This function rearranges the elements in an array so that the even elements come first before the odd elements
+
+    Assuming all elements are numbers
+
+    Time complexity: O(N)
+    Space Complexity: O(1)
+    """
+    next_even, next_odd = 0, len(arr)-1
+
+    while next_even < next_odd:
+        # check if the element is even number
+        if arr[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            # swap the element with the next_old element
+            arr[next_even], arr[next_odd] = arr[next_odd], arr[next_even]
+            next_odd -= 1
+
+
+def rearrange_slow(arr):
+    """
+    This function rearranges the elements in an array so that the even elements come first before the odd elements
+
+    Assuming all elements are numbers -> keeps the order of occurence
+
+    Time complexity: O(N)
+    Space Complexity: O(N)
+    """
+    even_arr = []
+    odd_arr = []
+
+    for value in arr:
+        if value % 2 == 0:
+            even_arr.append(value)
+        else:
+            odd_arr.append(value)
+
+    for i in range(len(even_arr)):
+        arr[i] = even_arr[i]
+
+    for i in range(len(odd_arr)):
+        arr[i + len(odd_arr)] = odd_arr[i]
+
     
 if __name__ == "__main__":
     array = DynamicArray()
     print(array.get_length())
-    array.insert(2)
-    array.insert(3)
-    array.insert(1)
-    array.insert(4)
+    array.insert(2, 0)
+    array.insert(3, 0)
+    array.insert(1, 1)
+    array.insert(4, 3)
+    array.insert(6, 3)
     array.print_array()
-    # print(array.get_length())
-    # print(array.get(3))
+    print(array.get_length())
+    print(array.get(3))
 
     array.delete(2)
     array.print_array()
+
+    arr = [1,2,3,4,5,6]
+    rearrange_slow(arr)
+    
+    print(arr)
