d92558cab6e74aea0e1afe0178db19a2a9a1637c
aobiegb <aobiegb@us.ibm.com>
2020-12-09 22:18:36 -0500

    added linear search implementation

---
+++ search/linearSearch.py
@@ -0,0 +1,20 @@
+def find(arr, target):
+    """
+    Big O(N)
+    """
+    if arr is None:
+        return
+    if not isinstance(arr, (list, tuple)):
+        raise TypeError(f"{arr} is not a list or tuple")
+
+    for i in range(len(arr)):
+        if target == arr[i]:
+            return True
+    
+    return False
+
+
+if __name__ == "__main__":
+    arr = [12, 5]
+
+    print(find(arr, 5))
