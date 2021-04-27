daa8c9a5a4dad1a649b115886d24f572a8be1537
aobiegb <aobiegb@us.ibm.com>
2021-04-27 08:05:33 -0400

    updated rotation code

--- arrays/arrayRotation.py
+++ arrays/arrayRotation.py
@@ -1,4 +1,8 @@
 def shift_left_rotate(arr, shifts):
+    """
+    Time complexity: O(N x shifts)
+    Space complexity: O(1)
+    """
     if len(arr) <= 1:
         return
 
@@ -7,9 +11,18 @@ def shift_left_rotate(arr, shifts):
 
 
 def shift_left(arr):
+    """
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
     temp = arr[0]
     for i in range(1, len(arr)):
         arr[i-1] = arr[i]
     arr[len(arr) - 1] = temp
 
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6]
+    shift_left_rotate(arr, 7)
+    print(arr)
 
